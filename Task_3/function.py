e7ea0ae20a635e6a4bd522e271bf7ea187edf42c
arun <arunkumar39874@gmail.com>
2024-01-25 15:49:38 +0530

    Commit

--- Task_3/function.py
+++ Task_3/function.py
@@ -1,4 +1,3 @@
-#import dependency
 import cv2
 import numpy as np
 import os
@@ -9,11 +8,11 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False                  # Image is no longer writeable
-    results = model.process(image)                 # Make prediction
-    image.flags.writeable = True                   # Image is now writeable 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False                 
+    results = model.process(image)                
+    image.flags.writeable = True                   
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     return image, results
 
 def draw_styled_landmarks(image, results):
@@ -32,7 +31,6 @@ def extract_keypoints(results):
       for hand_landmarks in results.multi_hand_landmarks:
         rh = np.array([[res.x, res.y, res.z] for res in hand_landmarks.landmark]).flatten() if hand_landmarks else np.zeros(21*3)
         return(np.concatenate([rh]))
-# Path for exported data, numpy arrays
 DATA_PATH = os.path.join('MP_Data') 
 
 actions = np.array(['A','B','C'])
